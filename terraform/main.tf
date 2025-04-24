58375cf9dd946948d95240c5bde6ed53ad5f775e
Aaron Luhning <a.luhning@vitalpoint.ai>
2025-04-24 09:53:40 -0400

    main update

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,11 @@ provider "aws" {
 resource "aws_key_pair" "nearflow_key" {
   key_name   = "nearflow-key"
   public_key = var.PROD_SSH_PUB_KEY
+
+  lifecycle {
+    ignore_changes  = [public_key]
+    prevent_destroy = true
+  }
 }
 
 resource "aws_security_group" "nearflow_sg" {
@@ -71,9 +76,12 @@ resource "aws_security_group" "nearflow_sg" {
       self             = false
     }
   ]
+
+  lifecycle {
+    create_before_destroy = true
+    prevent_destroy        = true
+  }
 }
-
-
 
 resource "aws_ebs_volume" "nearflow_volume" {
   availability_zone = "ca-central-1a"
@@ -91,31 +99,31 @@ resource "aws_instance" "nearflow_instance" {
   availability_zone      = "ca-central-1a"
   vpc_security_group_ids = [aws_security_group.nearflow_sg.id]
   user_data              = templatefile("nearflow-cloud-init.yaml", {
-    POSTGRES_USER               = var.POSTGRES_USER,
-    POSTGRES_PASSWORD           = var.POSTGRES_PASSWORD,
-    POSTGRES_DB                 = var.POSTGRES_DB,
-    LANGFLOW_SUPERUSER          = var.LANGFLOW_SUPERUSER,
-    LANGFLOW_SUPERUSER_PASSWORD = var.LANGFLOW_SUPERUSER_PASSWORD,
-    BACKEND_URL                 = var.BACKEND_URL,
-    LANGFLOW_AUTO_LOGIN         = var.LANGFLOW_AUTO_LOGIN,
-    LANGFLOW_CACHE_TYPE         = var.LANGFLOW_CACHE_TYPE,
-    LANGFLOW_CONFIG_DIR         = var.LANGFLOW_CONFIG_DIR,
-    LANGFLOW_DATABASE_URL       = var.LANGFLOW_DATABASE_URL,
-    LANGFLOW_ENV                = var.LANGFLOW_ENV,
-    LANGFLOW_HOST               = var.LANGFLOW_HOST,
-    LANGFLOW_LANGCHAIN_CACHE    = var.LANGFLOW_LANGCHAIN_CACHE,
-    LANGFLOW_LOG_FILE           = var.LANGFLOW_LOG_FILE,
-    LANGFLOW_LOG_LEVEL          = var.LANGFLOW_LOG_LEVEL,
-    LANGFLOW_OPEN_BROWSER       = var.LANGFLOW_OPEN_BROWSER,
-    LANGFLOW_PORT               = var.LANGFLOW_PORT,
-    LANGFLOW_REDIS_CACHE_EXPIRE = var.LANGFLOW_REDIS_CACHE_EXPIRE,
-    LANGFLOW_REDIS_DB           = var.LANGFLOW_REDIS_DB,
-    LANGFLOW_REDIS_HOST         = var.LANGFLOW_REDIS_HOST,
-    LANGFLOW_REDIS_PORT         = var.LANGFLOW_REDIS_PORT,
-    LANGFLOW_REMOVE_API_KEYS    = var.LANGFLOW_REMOVE_API_KEYS,
-    LANGFLOW_SAVE_DB_IN_CONFIG_DIR  = var.LANGFLOW_SAVE_DB_IN_CONFIG_DIR,
+    POSTGRES_USER                        = var.POSTGRES_USER,
+    POSTGRES_PASSWORD                    = var.POSTGRES_PASSWORD,
+    POSTGRES_DB                          = var.POSTGRES_DB,
+    LANGFLOW_SUPERUSER                   = var.LANGFLOW_SUPERUSER,
+    LANGFLOW_SUPERUSER_PASSWORD          = var.LANGFLOW_SUPERUSER_PASSWORD,
+    BACKEND_URL                          = var.BACKEND_URL,
+    LANGFLOW_AUTO_LOGIN                  = var.LANGFLOW_AUTO_LOGIN,
+    LANGFLOW_CACHE_TYPE                  = var.LANGFLOW_CACHE_TYPE,
+    LANGFLOW_CONFIG_DIR                  = var.LANGFLOW_CONFIG_DIR,
+    LANGFLOW_DATABASE_URL                = var.LANGFLOW_DATABASE_URL,
+    LANGFLOW_ENV                         = var.LANGFLOW_ENV,
+    LANGFLOW_HOST                        = var.LANGFLOW_HOST,
+    LANGFLOW_LANGCHAIN_CACHE             = var.LANGFLOW_LANGCHAIN_CACHE,
+    LANGFLOW_LOG_FILE                    = var.LANGFLOW_LOG_FILE,
+    LANGFLOW_LOG_LEVEL                   = var.LANGFLOW_LOG_LEVEL,
+    LANGFLOW_OPEN_BROWSER                = var.LANGFLOW_OPEN_BROWSER,
+    LANGFLOW_PORT                        = var.LANGFLOW_PORT,
+    LANGFLOW_REDIS_CACHE_EXPIRE          = var.LANGFLOW_REDIS_CACHE_EXPIRE,
+    LANGFLOW_REDIS_DB                    = var.LANGFLOW_REDIS_DB,
+    LANGFLOW_REDIS_HOST                  = var.LANGFLOW_REDIS_HOST,
+    LANGFLOW_REDIS_PORT                  = var.LANGFLOW_REDIS_PORT,
+    LANGFLOW_REMOVE_API_KEYS             = var.LANGFLOW_REMOVE_API_KEYS,
+    LANGFLOW_SAVE_DB_IN_CONFIG_DIR       = var.LANGFLOW_SAVE_DB_IN_CONFIG_DIR,
     LANGFLOW_STORE_ENVIRONMENT_VARIABLES = var.LANGFLOW_STORE_ENVIRONMENT_VARIABLES,
-    LANGFLOW_WORKERS            = var.LANGFLOW_WORKERS
+    LANGFLOW_WORKERS                     = var.LANGFLOW_WORKERS
   })
 
   tags = {
@@ -138,8 +146,8 @@ resource "aws_volume_attachment" "nearflow_attachment" {
 }
 
 resource "aws_eip" "nearflow_ip" {
-  instance = aws_instance.nearflow_instance.id
-  vpc      = true
+  instance   = aws_instance.nearflow_instance.id
+  vpc        = true
   depends_on = [aws_instance.nearflow_instance]
 }
 
@@ -156,51 +164,52 @@ resource "aws_route53_record" "nearflow_dns" {
   records = [aws_eip.nearflow_ip.public_ip]
 }
 
-resource "aws_iam_role" "nearflow_github_oidc_role" {
-  name = "nearflow-deploy-role"
+# ⚠️ COMMENTED OUT: IAM role must be bootstrapped manually to avoid circular dependency
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"
-        },
-        Action = "sts:AssumeRoleWithWebIdentity",
-        Condition = {
-          StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:ALuhning/nearflow:*"
-          },
-          StringEquals = {
-            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-          }
-        }
-      }
-    ]
-  })
-}
+# data "aws_caller_identity" "current" {}
 
-data "aws_caller_identity" "current" {}
+# resource "aws_iam_role" "nearflow_github_oidc_role" {
+#   name = "nearflow-deploy-role"
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Effect = "Allow",
+#         Principal = {
+#           Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"
+#         },
+#         Action = "sts:AssumeRoleWithWebIdentity",
+#         Condition = {
+#           StringLike = {
+#             "token.actions.githubusercontent.com:sub" = "repo:ALuhning/nearflow:*"
+#           },
+#           StringEquals = {
+#             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+#           }
+#         }
+#       }
+#     ]
+#   })
+# }
 
-resource "aws_iam_role_policy" "nearflow_deploy_policy" {
-  name = "nearflow-deploy-permissions"
-  role = aws_iam_role.nearflow_github_oidc_role.id
+# resource "aws_iam_role_policy" "nearflow_deploy_policy" {
+#   name = "nearflow-deploy-permissions"
+#   role = aws_iam_role.nearflow_github_oidc_role.id
 
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect   = "Allow",
-        Action   = [
-          "ec2:*",
-          "route53:*",
-          "iam:PassRole",
-          "acm:*",
-          "elasticloadbalancing:*"
-        ],
-        Resource = "*"
-      }
-    ]
-  })
-}
+#   policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Effect   = "Allow",
+#         Action   = [
+#           "ec2:*",
+#           "route53:*",
+#           "iam:PassRole",
+#           "acm:*",
+#           "elasticloadbalancing:*"
+#         ],
+#         Resource = "*"
+#       }
+#     ]
+#   })
+# }
